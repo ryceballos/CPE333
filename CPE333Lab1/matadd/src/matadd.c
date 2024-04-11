c1827b5841dc3fc02e83e44e100c8dacb837e8c2
Ryan Ceballos <rayito.marley@gmail.com>
2024-04-11 14:24:45 -0700

    Initial Lab 1

---
+++ CPE333Lab1/matadd/src/matadd.c
@@ -0,0 +1,14 @@
+#include "dataset.h"
+#include <stddef.h>
+
+/* NxM matrices */
+void matadd(int N, int M, const data_t A[], const data_t B[], data_t C[])
+{
+  int i, j;
+  
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) {
+      C[i*N + j] = A[i*N + j] + B[i*N + j];
+    }
+  }
+}
